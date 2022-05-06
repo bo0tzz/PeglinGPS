c7b45861fb7b74a6a0602da1e7ce2369d3f250db
bo0tzz <btdewilligen@gmail.com>
2022-05-06 14:59:13 +0200

    Update gamelibs latest peglin version

--- Patches/KillTweener.cs
+++ Patches/KillTweener.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using Battle;
 using DG.Tweening;
 using HarmonyLib;
 
--- Utils.cs
+++ Utils.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Battle;
+using UnityEngine;
 using Worldmap;
 
 namespace PeglinGPS
